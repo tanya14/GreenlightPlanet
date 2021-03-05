03b7e01520049bfd5c68ec7770bbea6ceb249212
tanya14 <Tanya.Sharma@smartenergywater.in>
2021-03-06 00:16:24 +0530

    Added cardview

--- app/src/main/java/com/example/greenlightplanet/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/example/greenlightplanet/ui/main/view/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.greenlightplanet.ui.main.view
 
 import android.os.Bundle
+import android.view.Menu
 import android.view.View
+import android.widget.SearchView
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -15,7 +17,7 @@ import com.example.greenlightplanet.ui.main.viewmodel.MainViewModel
 import com.example.greenlightplanet.utility.Status.*
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.item_layout.view.*
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -31,6 +33,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setupUI()
         setupObserver()
+        supportActionBar?.title = getString(R.string.country)
     }
 
     private fun setupUI() {
@@ -89,15 +92,19 @@ class MainActivity : AppCompatActivity() {
                 }
                 is Zone -> {
                     renderList(performances?.country)
+                    supportActionBar?.title = getString(R.string.country)
                 }
                 is Region -> {
                     renderList(zones)
+                    supportActionBar?.title = getString(R.string.zone)
                 }
                 is Area -> {
                     renderList(regions)
+                    supportActionBar?.title = getString(R.string.region)
                 }
                 is Employee -> {
                     renderList(areas)
+                    supportActionBar?.title = getString(R.string.area)
                 }
             }
         }
@@ -116,6 +123,7 @@ class MainActivity : AppCompatActivity() {
                 }
                 renderList(clickedTempList)
                 zones = clickedTempList
+                supportActionBar?.title = getString(R.string.zone)
             }
             is Zone -> {
                 val clickedTempList = arrayListOf<Region>()
@@ -128,6 +136,7 @@ class MainActivity : AppCompatActivity() {
                 }
                 renderList(clickedTempList)
                 regions = clickedTempList
+                supportActionBar?.title = getString(R.string.region)
             }
             is Region -> {
                 val clickedTempList = arrayListOf<Area>()
@@ -140,6 +149,7 @@ class MainActivity : AppCompatActivity() {
                 }
                 renderList(clickedTempList)
                 areas = clickedTempList
+                supportActionBar?.title = getString(R.string.area)
             }
             is Area -> {
                 val clickedTempList = arrayListOf<Employee>()
@@ -151,10 +161,31 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
                 renderList(clickedTempList)
+                supportActionBar?.title = getString(R.string.employee)
             }
             is Employee -> {
                 return
             }
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        val searchViewItem = menu?.findItem(R.id.search_bar)
+        val searchView = searchViewItem?.actionView as SearchView?
+
+        searchView?.isIconified = true
+        searchView?.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+
+        })
+        return true
     }
 }
