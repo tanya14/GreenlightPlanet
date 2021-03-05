86b9c0e51ad5ace67e7c7ea19175d6ce8b5d6f20
Paresh Krishna Sharma <emailtanyasharma@gmail.com>
2021-03-06 01:23:39 +0530

    Implemented Search View in Employee

--- app/src/main/java/com/example/greenlightplanet/ui/main/adapter/ListAdapter.kt
+++ app/src/main/java/com/example/greenlightplanet/ui/main/adapter/ListAdapter.kt
@@ -3,14 +3,19 @@ package com.example.greenlightplanet.ui.main.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.recyclerview.widget.RecyclerView
 import com.example.greenlightplanet.R
 import com.example.greenlightplanet.model.*
 import kotlinx.android.synthetic.main.item_layout.view.*
+import java.util.*
 
-class ListAdapter(onListItemClick: OnListItemClick) : RecyclerView.Adapter<ListAdapter.DataViewHolder>() {
+class ListAdapter(onListItemClick: OnListItemClick) :
+    RecyclerView.Adapter<ListAdapter.DataViewHolder>(), Filterable {
 
-    private var performances: List<*>? = null
+    private var performances: MutableList<Any>? = mutableListOf()
+    private var performancesFiltered: MutableList<Any>? = mutableListOf()
     private var onListItemClick: OnListItemClick? = null
 
     init {
@@ -56,15 +61,91 @@ class ListAdapter(onListItemClick: OnListItemClick) : RecyclerView.Adapter<ListA
     override fun onBindViewHolder(holder: DataViewHolder, position: Int) =
         holder.bind(performances?.get(position))
 
-    fun addData(list: List<*>) {
-        performances = list
+    fun addData(list: MutableList<Any>) {
+        performances?.clear()
+        performancesFiltered?.clear()
+        performances?.addAll(list)
+        performancesFiltered?.addAll(list)
     }
 
     fun getAdapterDataList(): List<*>? {
         return performances
     }
 
+    fun getAdapterItem(): Any? {
+        if (!performances.isNullOrEmpty()) {
+            return performances?.get(0)
+        }
+        return null
+    }
+
     interface OnListItemClick {
         fun onListItemClicked(clickedItem: Any?)
+    }
+
+    override fun getFilter(): Filter {
+        return listFilter
+    }
+
+    private val listFilter = object : Filter() {
+
+        override fun performFiltering(constraint: CharSequence?): FilterResults {
+            val filteredList: MutableList<Any> = mutableListOf()
+            if (constraint == null || constraint.isEmpty()) {
+                performancesFiltered?.let { filteredList.addAll(it) }
+            } else {
+                val filterPattern = constraint.toString().toLowerCase(Locale.US).trim()
+                if (performancesFiltered != null) {
+                    for (item in performancesFiltered!!) {
+                        when (item) {
+                            is Country -> {
+                                if (item.country?.toLowerCase(Locale.US)
+                                        ?.contains(filterPattern) == true
+                                ) {
+                                    filteredList.add(item)
+                                }
+                            }
+                            is Zone -> {
+                                if (item.zone?.toLowerCase(Locale.US)
+                                        ?.contains(filterPattern) == true
+                                ) {
+                                    filteredList.add(item)
+                                }
+                            }
+                            is Region -> {
+                                if (item.region?.toLowerCase(Locale.US)
+                                        ?.contains(filterPattern) == true
+                                ) {
+                                    filteredList.add(item)
+                                }
+                            }
+                            is Area -> {
+                                if (item.area?.toLowerCase(Locale.US)
+                                        ?.contains(filterPattern) == true
+                                ) {
+                                    filteredList.add(item)
+                                }
+                            }
+                            is Employee -> {
+                                if (item.name?.toLowerCase(Locale.US)
+                                        ?.contains(filterPattern) == true
+                                ) {
+                                    filteredList.add(item)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            val results = FilterResults()
+            results.values = filteredList
+            return results
+        }
+
+        override fun publishResults(p0: CharSequence?, p1: FilterResults?) {
+            performances?.clear()
+            performances = p1?.values as MutableList<Any>?
+            notifyDataSetChanged()
+        }
     }
 }
--- app/src/main/java/com/example/greenlightplanet/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/example/greenlightplanet/ui/main/view/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.greenlightplanet.ui.main.view
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.widget.SearchView
 import android.widget.Toast
 import androidx.activity.viewModels
@@ -27,6 +29,7 @@ class MainActivity : AppCompatActivity() {
     private var zones: ArrayList<Zone>? = ArrayList()
     private var regions: ArrayList<Region>? = ArrayList()
     private var areas: ArrayList<Area>? = ArrayList()
+    private var searchViewItem: MenuItem? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -79,35 +82,33 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun renderList(performances: List<Any>?) {
-        performances?.let { adapter.addData(it) }
+        performances?.let { adapter.addData(it.toMutableList()) }
         adapter.notifyDataSetChanged()
     }
 
     override fun onBackPressed() {
-        val tempList: List<*>? = adapter.getAdapterDataList()
-        if (tempList?.isNotEmpty() == true) {
-            when (tempList[0]) {
-                is Country -> {
-                    finishAffinity()
-                }
-                is Zone -> {
-                    renderList(performances?.country)
-                    supportActionBar?.title = getString(R.string.country)
-                }
-                is Region -> {
-                    renderList(zones)
-                    supportActionBar?.title = getString(R.string.zone)
-                }
-                is Area -> {
-                    renderList(regions)
-                    supportActionBar?.title = getString(R.string.region)
-                }
-                is Employee -> {
-                    renderList(areas)
-                    supportActionBar?.title = getString(R.string.area)
-                }
+        when (adapter.getAdapterItem()) {
+            is Country -> {
+                finishAffinity()
+            }
+            is Zone -> {
+                renderList(performances?.country)
+                supportActionBar?.title = getString(R.string.country)
+            }
+            is Region -> {
+                renderList(zones)
+                supportActionBar?.title = getString(R.string.zone)
+            }
+            is Area -> {
+                renderList(regions)
+                supportActionBar?.title = getString(R.string.region)
+            }
+            is Employee -> {
+                renderList(areas)
+                supportActionBar?.title = getString(R.string.area)
             }
         }
+        searchViewItem?.isVisible = adapter.getAdapterItem() is Employee
     }
 
     private fun renderListAfterItemClicked(clickedItem: Any?) {
@@ -163,26 +164,27 @@ class MainActivity : AppCompatActivity() {
                 renderList(clickedTempList)
                 supportActionBar?.title = getString(R.string.employee)
             }
-            is Employee -> {
-                return
-            }
         }
+        searchViewItem?.isVisible = adapter.getAdapterItem() is Employee
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater = menuInflater
         inflater.inflate(R.menu.menu, menu)
-        val searchViewItem = menu?.findItem(R.id.search_bar)
+        searchViewItem = menu?.findItem(R.id.search_bar)
         val searchView = searchViewItem?.actionView as SearchView?
+        searchViewItem?.isVisible = adapter.getAdapterItem() is Employee
 
         searchView?.isIconified = true
-        searchView?.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+        searchView?.imeOptions = EditorInfo.IME_ACTION_DONE
+        searchView?.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
-                return true
+                return false
             }
 
             override fun onQueryTextChange(newText: String?): Boolean {
-                return true
+                adapter.filter.filter(newText)
+                return false
             }
 
         })
